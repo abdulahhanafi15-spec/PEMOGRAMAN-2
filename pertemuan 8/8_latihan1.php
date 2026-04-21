c8fc4dd8f8ff2905bd11eef34c3fc4a092b01e58
Abdulah Hanafi <abdulahhanafi15@gmail.com>
2026-04-21 13:39:50 +0700

    Add files via upload

---
+++ pertemuan 8/8_latihan1.php
@@ -0,0 +1,33 @@
+<?php
+
+function basic($argument)
+{
+    echo $argument;
+}
+
+basic('hello world!'); // outputs: hello world!
+
+?>
+
+
+2.
+<html>
+<head>
+    <title>Contoh Penggunaan UDF</title>
+</head>
+
+<body>
+
+<!-- Form Input -->
+<form method="post">
+
+Masukkan Bilangan Pertama : <br>
+<input type="text" name="A" size="10"><br>
+
+Masukkan Bilangan Kedua : <br>
+<input type="text" name="B" size="10"><br><br>
+
+<input type="submit" value="Hitung">
+
+</form>
+
